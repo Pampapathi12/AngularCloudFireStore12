b2179b45637805e5f93eeb75588a77de4e925c9c
Pampapathi12 <pampapahik12@gmail.coom>
2022-10-01 00:04:09 +0530

    Employee crud operation

---
+++ EmployeeCrudOperations/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'employees-e353e',
+    appId: '1:779865677076:web:b4c2f4f258e764ac31b601',
+    storageBucket: 'employees-e353e.appspot.com',
+    locationId: 'asia-east1',
+    apiKey: 'AIzaSyDo2e9LY85A-Xe0C-paWC6G275Ww4HH5Hg',
+    authDomain: 'employees-e353e.firebaseapp.com',
+    messagingSenderId: '779865677076',
+    measurementId: 'G-55LLTS8WG6',
+  },
+};
---
+++ EmployeeCrudOperations/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'employees-e353e',
+    appId: '1:779865677076:web:b4c2f4f258e764ac31b601',
+    storageBucket: 'employees-e353e.appspot.com',
+    locationId: 'asia-east1',
+    apiKey: 'AIzaSyDo2e9LY85A-Xe0C-paWC6G275Ww4HH5Hg',
+    authDomain: 'employees-e353e.firebaseapp.com',
+    messagingSenderId: '779865677076',
+    measurementId: 'G-55LLTS8WG6',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
